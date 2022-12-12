5da17c253af97729287df1bd26e296e60aaa754b
aeither <36173828+aeither@users.noreply.github.com>
2022-12-12 14:08:28 +0100

    add listings

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,15 +2,21 @@ import { MediaRenderer } from "@thirdweb-dev/react";
 import type { FC } from "react";
 
 export interface CardProps {
-  title: string;
+  title: string | number;
+  subtitle: string;
   description: string;
   image: string;
-  btnText: string;
   disabled?: boolean;
   btnAction: () => void;
 }
 
-const Card: FC<CardProps> = ({ title, description, image, btnAction }) => {
+const Card: FC<CardProps> = ({
+  title,
+  subtitle,
+  description,
+  image,
+  btnAction,
+}) => {
   return (
     <div className="group card w-auto border border-neutral-medium bg-neutral-dark transition duration-300 hover:bg-neutral-medium">
       <figure>
@@ -21,9 +27,11 @@ const Card: FC<CardProps> = ({ title, description, image, btnAction }) => {
         />
       </figure>
       <div className="card-body">
+        <p>
+          <b>{subtitle}</b>
+        </p>
         <h2 className="card-title text-white">{title}</h2>
         <p className="truncate text-neutral-light">{description}</p>
-        {/* <div className="card-actions justify-end"></div> */}
       </div>
       <div className="rounded-b-lg border-t border-t-slate-500 bg-neutral-medium p-2">
         <button onClick={() => btnAction()} className="w-full font-bold">
---
+++ src/components/Listing.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import {
+  useActiveListings,
+  useContract,
+  useNetwork,
+  useNetworkMismatch,
+} from "@thirdweb-dev/react";
+import { ChainId } from "@thirdweb-dev/sdk/evm";
+import { FC } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { MARKETPLACE_ADDRESS } from "../utils/constants";
+import Card from "./Card";
+
+const Listing: FC = () => {
+  const networkMismatch = useNetworkMismatch();
+  const [, switchNetwork] = useNetwork();
+  const { contract: marketplace } = useContract(
+    MARKETPLACE_ADDRESS,
+    "marketplace"
+  );
+
+  const { data: listings, isLoading: loadingListings } =
+    useActiveListings(marketplace);
+
+  async function buyNft(listingId: string) {
+    try {
+      // Ensure user is on the correct network
+      if (networkMismatch) {
+        switchNetwork && switchNetwork(ChainId.Goerli);
+        return;
+      }
+
+      // Simple one-liner for buying the NFT
+      await marketplace?.buyoutListing(listingId, 1);
+      toast("NFT bought successfully!");
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  }
+
+  return (
+    <>
+      <Toaster />
+      <div className="grid grid-cols-5 gap-4">
+        {loadingListings ? (
+          <div>Loading listings...</div>
+        ) : (
+          <div className="span-1">
+            {listings?.map((listing) => (
+              <div key={listing.id}>
+                <Card
+                  title={listing.asset.name || ""}
+                  subtitle={
+                    listing.buyoutCurrencyValuePerToken.displayValue +
+                    " " +
+                    listing.buyoutCurrencyValuePerToken.symbol
+                  }
+                  description={listing.asset.description || ""}
+                  image={listing.asset.image || ""}
+                  btnAction={() => buyNft(listing.id)}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Listing;
